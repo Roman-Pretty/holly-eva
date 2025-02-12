19bf3767b8a0529076b058384071f0fdff93150e
Roman Pretty <rjjpretty@gmail.com>
2025-02-12 00:45:04 +0000

    fixed styling for mobile

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="hero bg-base-200 sm:min-h-[calc(100vh-4rem)] min-h-[90vh] flex items-center justify-center">
+    <div class="hero bg-base-200 min-h-[calc(100dvh-4rem)] flex flex-col items-center xs:justify-center xs-pt-0 pt-[20%]">
         <div class="text-start hero-content flex-col items-center px-0 mx-0">
             <img src="../assets/holly.jpg" alt="music" class="lg:max-w-[30vw] md:max-w-[50vw] sm:max-w-[60vw] max-w-full" />
             <img src="../assets/hollylogo.svg" alt="music" class="lg:max-w-[46vw] md:max-w-[50vw] sm:max-w-[60vw]" />
