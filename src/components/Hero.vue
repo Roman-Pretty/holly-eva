9603de9b14988030354acff18e4caf1651dfd4e4
Roman Pretty <rjjpretty@gmail.com>
2025-02-12 00:34:34 +0000

    minor fix

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="hero bg-base-200 min-h-[calc(100vh-4rem)] flex items-center justify-center">
-        <div class="text-start hero-content flex-col px-0 mx-0">
+    <div class="hero bg-base-200 min-h-[calc(100dvh-4rem)] flex items-center justify-center">
+        <div class="text-start hero-content flex-col items-center px-0 mx-0">
             <img src="../assets/holly.jpg" alt="music" class="lg:max-w-[30vw] md:max-w-[50vw] sm:max-w-[60vw] max-w-full" />
             <img src="../assets/hollylogo.svg" alt="music" class="lg:max-w-[46vw] md:max-w-[50vw] sm:max-w-[60vw]" />
             <!-- <h1 class="md:text-5xl text-4xl xl:text-[10rem] font-bold font-serif italic uppercase mt-10">Holly Eva</h1> -->
